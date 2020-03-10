fee7a1032a033da5613822b12e69a2c3fc28de87
bosollee <bosollee@gmail.com>
2020-03-10 22:33:23 +0900

    Add PostList UI, API with Redux

--- my-world-frontend/src/pages/PostListPage.js
+++ my-world-frontend/src/pages/PostListPage.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import HeaderContainer from '../containers/common/HeaderContainer';
+import PostListContainer from '../containers/posts/PostListContainer';
 
 const PostListPage = () => {
   return (
     <>
       <HeaderContainer />
-      <div>Hi, I'm POST LIST.</div>
+      <PostListContainer />
     </>
   );
 };
